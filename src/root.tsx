486cf1433f792833c4c491a9c7d9114dea13656a
LloydBistany <lloydbistany@gmail.com>
2023-04-07 12:17:59 -0400

    Admin Page is only visible to github users whose emails are saved in the .env file

--- src/root.tsx
+++ src/root.tsx
@@ -1,5 +1,5 @@
 // @refresh reload
-import { Suspense } from "solid-js";
+import { Suspense, createResource } from "solid-js";
 import {
   useLocation,
   A,
@@ -12,16 +12,77 @@ import {
   Routes,
   Scripts,
   Title,
+  useSearchParams,
 } from "solid-start";
+import { getSession } from "@auth/solid-start";
+import { createServerData$ } from "solid-start/server";
+import { authOpts } from "./routes/api/[...solidAuth]";
+import { signIn, signOut } from "@auth/solid-start/client";
+import { Switch, Match, Show, createSignal, createEffect} from "solid-js";
 import "./root.css";
 
 
 export default function Root() {
+
+
   const location = useLocation();
   const active = (path: string) =>
     path == location.pathname
       ? "border-sky-600"
       : "border-transparent hover:border-sky-600";
+
+
+  //pull Client ID from .env file, which is passed as an endpoint when "Admin Login" is clicked
+  const clientId: string = import.meta.env.VITE_ClientID;
+  //access the code given to us by github after a login, which will be used to gain an access token 
+  const [searchParams] = useSearchParams();
+  const code = searchParams.code;
+
+  const useSession = () => {
+    return createServerData$(
+      async (_, { request }) => {
+        return await getSession(request, authOpts)
+      },
+      { key: () => ["auth_user"] }
+    )
+  }
+
+  const session = useSession();
+  const user = () => session()?.user;
+  
+  
+
+  const login = () => signIn("github");
+  const logout = () => signOut();
+
+
+  //Admin Login Checker
+  const [adminLogin, setAdminLogin] = createSignal(false);
+  const [username, setUsername] = createSignal(user()?.email);
+ 
+
+  //four Admins to check
+  const admin1: string = import.meta.env.VITE_Admin1;
+  const admin2: string = import.meta.env.VITE_Admin2;
+  const admin3: string = import.meta.env.VITE_Admin3;
+  const admin4: string = import.meta.env.VITE_Admin4;
+
+  createEffect(() => {
+      console.log("This is the user email:", username());
+      if (
+        username() === admin1 ||
+        username() === admin2 ||
+        username() === admin3 ||
+        username() === admin4
+      ) {
+        setAdminLogin(true);
+      }
+    })
+
+    //Send username to database; 
+    //Create isUser signal in Admin to check if the user is the right user. 
+
+
   return (
     <Html lang="en">
       <Head>
@@ -34,7 +95,7 @@ export default function Root() {
           <ErrorBoundary>
             <nav class="bg-gray-800 flex flex-row">
               <ul class="flex items-center p-3 text-gray-200">
-              <img class="h-12 w-12" src="/SquidLogo.png" />
+                <img class="h-12 w-12" src="/SquidLogo.png" />
                 <li class={`border-b-2 ${active("/")} mx-1.5 sm:mx-6`}>
                   <A href="/">Home</A>
                 </li>
@@ -44,16 +105,39 @@ export default function Root() {
                 <li class={`border-b-2 ${active("/team")} mx-1.5 sm:mx-6`}>
                   <A href="/team">Team</A>
                 </li>
-                </ul>
-                <div class="flex flex-grow justify-end">
-                <button class="flex flex-row items-center ">
-                  <img class="h-8 w-8" src="/Medium.png"></img>
-                  <img class="h-8 w-8 mx-3" src="/GithubWhite.png"/>
-                  <div class={` border-b-2 text-gray-200 ${active("https://github.com/")} mx-1.5 sm:mx-6`}>
-                  <A href="https://github.com/">Admin Login</A> 
+                <Show when={adminLogin() === true}>
+                  <li class={`border-b-2 ${active("/admin")} mx-1.5 sm:mx-6`}>
+                    <A href="/admin">Admin</A>
+                  </li>
+                </Show>
+              </ul>
+              <div class="flex flex-grow justify-end">
+                <button class="flex flex-row items-center">
+                  <img class="h-8 w-8 mr-3" src="/yarnBlackWithWhiteLogo.png"></img>
+                  <img class="h-14 w-14 mr-3" src="/npmBlackIcon.png"></img>
+                  <img class="h-8 w-8 mr-4" src="/Medium.png"></img>
+                  <img class="h-8 w-8 " src="/githubBlackWithWhite.png" />
+                  <div class={` border-b-2 text-gray-200 ${active("https://github.com/")} sm:mx-6`}>
+                    <Switch >
+                      <Match when={user()}>
+                        <div>Logged in as {user()?.name}</div>
+                        <button onClick={() => {
+                          logout()
+                          setUsername('');
+                          setAdminLogin(false);
+                        }
+                        }>Logout</button>
+                      </Match>
+                      <Match when={!user()}>
+                        <button class="border" onClick={() => {
+                          login();
+                          setUsername(user()?.email);
+                        }}>Admin Login</button>
+                      </Match>
+                    </Switch>
                   </div>
                 </button>
-                </div>
+              </div>
             </nav>
             <Routes>
               <FileRoutes />
