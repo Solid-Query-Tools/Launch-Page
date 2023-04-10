c2b41b20c9bb9a553d4b5e58f3fb7992e28224af
Dakota McPherson <dakotalmcpherson@gmail.com>
2023-04-10 18:43:05 -0400

    some backend and frontend for feedback

--- server/models.ts
+++ server/models.ts
@@ -1,4 +1,4 @@
-import { Schema, connect, model } from 'mongoose';
+const { Schema, connect, model } = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config();
@@ -11,23 +11,32 @@ connect(MONGO_URI)
 
 // TO DO: 
 // initialize models and schemas
-interface IFeedback {
-  type: string,
-  message: string,
-  createdBy: string,
-  createdAt: Date,
-  approved: boolean,
-  adminResponse: string
-}
+// interface IFeedback {
+//   type: string,
+//   message: string,
+//   createdBy: string,
+//   createdAt: Date,
+//   approved: boolean,
+//   adminResponse: string
+// }
 
-const feedbackSchema = new Schema<IFeedback>({
+const feedbackSchema = new Schema({
   type: {type: String, required: true},
   message: {type: String, required: true},
   createdBy: {type: String, required: true}, //Could be a user instance instead
   createdAt: {type: Date, required: true}, 
   approved: {type: Boolean, default: false, required: true},
   adminResponse: {type: String, required: false}, 
-})
+});
+
+const userSchema = new Schema({
+  username: {type: String, required: true},
+  email: {type: String, required: false},
+  admin: {type: Boolean, requred: true, default: false}
+});
 
 // export models
-export const Feedback = model<IFeedback>('feedback', feedbackSchema);
+const Feedback = model('feedback', feedbackSchema);
+const User = model('user', userSchema);
+
+module.exports = { Feedback, User };
