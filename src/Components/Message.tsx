9fbf5667e847a3e629cc7bc1972aa80033a9591e
Dakota McPherson <dakotalmcpherson@gmail.com>
2023-04-10 19:59:23 -0400

    working on message subcomponent

--- src/Components/Message.tsx
+++ src/Components/Message.tsx
@@ -1,5 +1,12 @@
 export default function Message(props) {
+  console.log(props)
   return (
-    <div>test</div>
+    <div class="h-10 bg-white w-4/5 text-black">
+      <article>
+        <span>{props.createdBy}</span>
+        <span>{props.createdAt}</span>
+      </article>
+      
+    </div>
   )
 }
