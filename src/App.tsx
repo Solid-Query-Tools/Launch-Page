435324fc08fe2640d0548b76520df7f022e919e1
Truett <truett.davis@gmail.com>
2023-04-14 19:19:51 -0400

    implemented responsive styling

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ import BackgroundBubbles from "./assets/BackgroundBubbles.mp4"
 import SolidLogo from "./assets/SolidLogo.png"
 import Header from './header';
 import { A } from '@solidjs/router';
-import Testimonial from './testimonial';
 
 const App: Component = () => {
   const [view, setView] = createSignal('install')
@@ -29,13 +28,18 @@ const App: Component = () => {
     }
   }
 
+  const styling = switcher =>
+  view() == switcher
+    ? "border-sky-600"
+    : "border-transparent hover:border-sky-600";
+
   return (
     <>
       <Header />
-      <main class="relative flex mb-12 overflow-hidden h-64">
-        <div class="relative flex flex-row justify-center items-center z-30 p-5 text-6xl justify-center text-sky-200 text-center bg-opacity-50 rounded-xl w-screen">
-          <img class="h-14 w-14" src={SolidLogo}></img>
-          olid Query Devtools
+      <main class="relative flex sm:mb-12 overflow-hidden h-32 sm:h-64">
+        <div class="relative flex flex-col justify-center items-center z-30 p-5 text-3xl sm:text-6xl justify-center text-sky-200 text-center bg-opacity-50 rounded-xl w-screen">
+          <img class="h-7 w-7 sm:h-14 sm:w-14" src={SolidLogo} />
+          Solid Query Devtools
         </div>
         <video
           autoplay
@@ -48,63 +52,61 @@ const App: Component = () => {
             type="video/mp4"
           />
         </video>
-
       </main>
-
       <div class="flex flex-col">
-        <p class="text-center font-bold">
+        <p class="text-center font-thin md:relative md:mb-10">
         A lightweight NPM package that allows you to debug SolidJS applications using Tanstack's Solid Query.
         </p>
-        <div class="flex justify-around mt-10 h-[20em]">
-            <div class="bg-gray-800 w-1/3 rounded-xl">
-              <h1 class="max-6-xs text-6xl text-sky-700 font-thin p-5">
-                Features
-              </h1>
-              <ul class="list-outside list-disc text-sm">
-                <li>analyze information about query status, including whether a query is fresh, stale, paused, or inactive</li>
-                <li>sort queries by query keys and latest updated</li>
-                <li>refetch or remove queries</li>
-                <li>review the data received from each query as well as metadata about query update times and status</li>
-              </ul>
+        <div class="flex flex-col lg:flex-row justify-around mt-10 sm:h-[20em]">
+          <div class="bg-gray-800 lg:w-1/3 rounded-xl flex flex-col m-2">
+            <h1 class="text-3xl sm:text-6xl text-sky-700 font-thin p-5">
+              Features
+            </h1>
+            <ul class="list-outside list-disc text-sm px-9 font-thin">
+              <li class="py-1">analyze information about query status, including whether a query is fresh, stale, paused, or inactive</li>
+              <li class="py-1">sort queries by query keys and latest updated</li>
+              <li class="py-1">refetch or remove queries</li>
+              <li class="py-1">review the data received from each query as well as metadata about query update times and status</li>
+            </ul>
+          </div>
+          <div class="lg:w-1/3 min-w-fit bg-gray-800 rounded-xl m-2">
+            <div class="flex justify-between">
+              <h1 onClick={() => setView('install')} class={`border-b-2 mx-1.5 sm:mx-6 ${styling('install')} hover:cursor-pointer max-6-xs text-3xl sm:text-6xl text-sky-700 font-thin p-5`}>Install</h1>
+              <h1 onClick={() => setView('how')} class={`border-b-2 mx-1.5 sm:mx-6 ${styling('how')} hover:cursor-pointer max-6-xs text-3xl sm:text-6xl text-sky-700 font-thin p-5`}>How to Use</h1>
             </div>
-        <div class="w-1/3 min-w-fit bg-gray-800 rounded-xl">
-          <div class="flex justify-between">
-          <h1 onClick={() => setView('install')} class="hover:cursor-pointer max-6-xs text-6xl text-sky-700 font-thin p-5">Install</h1>
-          <h1 onClick={() => setView('how')} class="hover:cursor-pointer max-6-xs text-6xl text-sky-700 font-thin p-5">How to Use</h1>
-          </div>
-          <Show when={view() == 'install'}>
-          <div class="flex flex-col items-center">
-          <div onMouseOver={() => setCopyNPM(true)}  onMouseLeave={() => setCopyNPM(false)}class="rounded-lg px-2 flex items-center justify-between mt-3 bg-gray-700 w-3/5 p-1 font-['Roboto-mono']">
-            <p>npm install solid-query-devtools</p>
-            <Show when={copyNPM() || copiedNPM()}>
-              <Switch>
-                <Match when={!copiedNPM()}>
-                  <IoCopy class='cursor-pointer' size={18} color={"gray"} title="CopyOutlined" onClick={() => copyText('npm install solid-query-devtools', setCopiedNPM)}/>
-                </Match>
-                <Match when={copiedNPM()}>
-                  <FaRegularCircleCheck size={18} color={"green"} title="CopyOutlined"/>
-                </Match>
-              </Switch>
+            <Show when={view() == 'install'}>
+              <div class="flex flex-col items-center">
+                <div onMouseOver={() => setCopyNPM(true)}  onMouseLeave={() => setCopyNPM(false)}class="rounded-lg px-2 flex items-center justify-between mt-10 bg-gray-700 w-3/5 p-1 font-['Roboto-mono']">
+                  <p class="font-mono text-sm px-2">npm install solid-query-devtools</p>
+                  <Show when={copyNPM() || copiedNPM()}>
+                    <Switch>
+                      <Match when={!copiedNPM()}>
+                        <IoCopy class='cursor-pointer' size={18} color={"gray"} title="CopyOutlined" onClick={() => copyText('npm install solid-query-devtools', setCopiedNPM)}/>
+                      </Match>
+                      <Match when={copiedNPM()}>
+                        <FaRegularCircleCheck size={18} color={"green"} title="CopyOutlined"/>
+                      </Match>
+                    </Switch>
+                  </Show>
+                </div>
+                <div onMouseOver={() => setCopyYarn(true)}  onMouseLeave={() => setCopyYarn(false)} class="rounded-lg px-2 mb-2 flex items-center justify-between mt-3 bg-gray-700 w-3/5 p-1 font-['Roboto-mono']">
+                  <p class="font-mono text-sm px-2">yarn add @solid-query-devtools</p>
+                  <Show when={copyYarn() || copiedYarn()}>
+                    <Switch>
+                      <Match when={!copiedYarn()}>
+                        <IoCopy class='cursor-pointer' size={18} color={"gray"} title="CopyOutlined" onClick={() => copyText('yarn add @solid-query-devtools', setCopiedYarn)}/>
+                      </Match>
+                      <Match when={copiedYarn()}>
+                        <FaRegularCircleCheck size={18} color={"green"} title="CopyOutlined"/>
+                      </Match>
+                    </Switch>
+                  </Show>
+                </div>
+              </div>
             </Show>
-          </div>
-          <div onMouseOver={() => setCopyYarn(true)}  onMouseLeave={() => setCopyYarn(false)} class="rounded-lg px-2 flex items-center justify-between mt-3 bg-gray-700 w-3/5 p-1 font-['Roboto-mono']">
-            <p>yarn add @solid-query-devtools</p>
-            <Show when={copyYarn() || copiedYarn()}>
-              <Switch>
-                <Match when={!copiedYarn()}>
-                  <IoCopy class='cursor-pointer' size={18} color={"gray"} title="CopyOutlined" onClick={() => copyText('yarn add @solid-query-devtools', setCopiedYarn)}/>
-                </Match>
-                <Match when={copiedYarn()}>
-                  <FaRegularCircleCheck size={18} color={"green"} title="CopyOutlined"/>
-                </Match>
-              </Switch>
-            </Show>
-          </div>
-          </div>
-          </Show>
           </div>
         </div>
-        <p class="my-4">
+        <p class="my-4 ml-5 relative bottom-0 sm:fixed lg:relative">
           <span>Home</span>
           {" - "}
           <A href="/about" class="text-sky-600 hover:underline">
--- src/header.tsx
+++ src/header.tsx
@@ -11,41 +11,44 @@ export default function Header() {
 
   const oauth = () => {
     window.location.href = `https://github.com/login/oauth/authorize?client_id=${client_id}`
-  }
+  };
 
-    const location = useLocation();
-    const active = (path: string) =>
+  const location = useLocation();
+  const active = (path: string) =>
     path == location.pathname
-      ? "border-sky-600"
-      : "border-transparent hover:border-sky-600";
+    ? "border-sky-600"
+    : "border-transparent hover:border-sky-600";
 
-    return (
-        <>
-            <nav class="bg-gray-800 flex flex-row sticky top-0 z-40">
-                <ul class="flex items-center p-3 text-gray-200">
-                    <img class="h-12 w-12" src={SquidLogo} />
-                    <li class={`border-b-2 ${active("/")} mx-1.5 sm:mx-6`} >
-                        <a href="/">Home</a>
-                    </li>
-                    <li class={`border-b-2 ${active("/team")} mx-1.5 sm:mx-6`} >
-                        <a href="/team">Team</a>
-                    </li>
-                    <li class={`border-b-2 ${active("/feedback")} mx-1.5 sm:mx-6`}>
-                        <a href="/feedback">Feedback</a>
-                    </li>
-                </ul>
-                <div class="flex flex-grow justify-end">
-                    <div class="flex flex-row items-center">
-                        <img class="h-10 w-10 mr-3" src={NPMLogo}></img>
-                        <img class="h-8 w-8 mr-3" src={YarnLogo}></img>
-                        <img class="h-8 w-8 mr-4" src={MediumLogo}></img>
-                        <img class="h-8 w-8 " src={GithubLogo} />
-                        <div class={`border-b-2 ${active("https://github.com/")} mx-1.5 sm:mx-6`} >
-                            <button onClick={() => oauth()}>Admin Login</button>
-                        </div>
-                    </div>
-                </div>
-            </nav>
-        </>
-    )
+  return (
+    <>
+      <nav class="bg-gray-800 flex flex-row sticky top-0 z-40 max-h-20">
+        <ul class="flex items-center p-3 text-gray-200">
+          <img class="h-12 w-12" src={SquidLogo} />
+          <li class={`border-b-2 ${active("/")} mx-1.5 sm:mx-6`} >
+            <a href="/">Home</a>
+          </li>
+          <li class={`border-b-2 ${active("/team")} mx-1.5 sm:mx-6`} >
+            <a href="/team">Team</a>
+          </li>
+          <li class={`border-b-2 ${active("/feedback")} mx-1.5 sm:mx-6`}>
+            <a href="/feedback">Feedback</a>
+          </li>
+          <li class={`border-2 rounded p-2 ml-10 visible text-[10px] absolute right-2 sm:invisible sm:mx-6`}>
+            <button onClick={() => oauth()}>Github Login</button>
+          </li>
+        </ul>
+        <div class="flex flex-grow justify-end">
+          <div class="sm:flex sm:flex-row sm:items-center">
+            <img class="h-10 w-10 mr-3 invisible sm:visible" src={NPMLogo} />
+            <img class="h-8 w-8 mr-3 invisible sm:visible" src={YarnLogo} />
+            <img class="h-8 w-8 mr-4 invisible sm:visible" src={MediumLogo} />
+            <img class="h-8 w-8 invisible sm:visible" src={GithubLogo} />
+            <div class={`border-b-2 ${active("https://github.com/")} mx-1.5 invisible sm:visible sm:mx-6`}>
+              <button onClick={() => oauth()}>Login with Github</button>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
+  )
 }
