e3963b638a3442e48be40c51033229cec77b5052
Dakota McPherson <dakotalmcpherson@gmail.com>
2023-04-14 11:02:42 -0400

    nearly completed feedback page, began work on home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import type { Component } from 'solid-js';
+import { Component, createSignal } from 'solid-js';
 import YarnLogo from "./assets/YarnLogo.png"
 import SquidLogo from "./assets/SquidLogo.png"
+import { IoCopy } from 'solid-icons/io'
+import { FaRegularCircleCheck } from 'solid-icons/fa'
 import BackgroundBubbles from "./assets/BackgroundBubbles.mp4"
 import SolidLogo from "./assets/SolidLogo.png"
 import Header from './header';
@@ -8,6 +10,25 @@ import { A } from '@solidjs/router';
 import Testimonial from './testimonial';
 
 const App: Component = () => {
+  const [view, setView] = createSignal('install')
+  const [copyNPM, setCopyNPM] = createSignal(false)
+  const [copyYarn, setCopyYarn] = createSignal(false)
+  const [copiedNPM, setCopiedNPM] = createSignal(false)
+  const [copiedYarn, setCopiedYarn] = createSignal(false)
+
+  async function copyText(str, setSignal) {
+    try {
+      await navigator.clipboard.writeText(str);
+      setSignal(true)
+      setTimeout(() => {
+        setSignal(false)
+      }, 2000);
+    }
+    catch (err) {
+      console.log('error')
+    }
+  }
+
   return (
     <>
       <Header />
@@ -30,17 +51,59 @@ const App: Component = () => {
 
       </main>
 
-      <div class="">
+      <div class="flex flex-col">
+        <p class="text-center font-bold">
         A lightweight NPM package that allows you to debug SolidJS applications using Tanstack's Solid Query.
-        <h1 class="max-6-xs text-6xl text-sky-700 font-thin my-16">
+        </p>
+        <div class="flex justify-around mt-10 h-[20em]">
+        <div class="bg-gray-800 w-1/3 rounded-xl">
+        <h1 class="max-6-xs text-6xl text-sky-700 font-thin p-5">
           Features
         </h1>
-        <ul>
-          <li>- analyze information about query status, including whether a query is fresh, stale, paused, or inactive</li>
-          <li>- sort queries by query keys and latest updated</li>
-          <li>- refetch or remove queries</li>
-          <li>- review the data received from each query as well as metadata about query update times and status</li>
+        <ul class="list-outside list-disc text-sm">
+          <li>analyze information about query status, including whether a query is fresh, stale, paused, or inactive</li>
+          <li>sort queries by query keys and latest updated</li>
+          <li>refetch or remove queries</li>
+          <li>review the data received from each query as well as metadata about query update times and status</li>
         </ul>
+        </div>
+        <div class="w-1/3 min-w-fit bg-gray-800 rounded-xl">
+          <div class="flex justify-between">
+          <h1 onClick={() => setView('install')} class="hover:cursor-pointer max-6-xs text-6xl text-sky-700 font-thin p-5">Install</h1>
+          <h1 onClick={() => setView('how')} class="hover:cursor-pointer max-6-xs text-6xl text-sky-700 font-thin p-5">How to Use</h1>
+          </div>
+          <Show when={view() == 'install'}>
+          <div class="flex flex-col items-center">
+          <div onMouseOver={() => setCopyNPM(true)}  onMouseLeave={() => setCopyNPM(false)}class="rounded-lg px-2 flex items-center justify-between mt-3 bg-gray-700 w-3/5 p-1 font-['Roboto-mono']">
+            <p>npm install solid-query-devtools</p>
+            <Show when={copyNPM() || copiedNPM()}>
+              <Switch>
+                <Match when={!copiedNPM()}>
+                  <IoCopy class='cursor-pointer' size={18} color={"gray"} title="CopyOutlined" onClick={() => copyText('npm install solid-query-devtools', setCopiedNPM)}/>
+                </Match>
+                <Match when={copiedNPM()}>
+                  <FaRegularCircleCheck size={18} color={"green"} title="CopyOutlined"/>
+                </Match>
+              </Switch>
+            </Show>
+          </div>
+          <div onMouseOver={() => setCopyYarn(true)}  onMouseLeave={() => setCopyYarn(false)} class="rounded-lg px-2 flex items-center justify-between mt-3 bg-gray-700 w-3/5 p-1 font-['Roboto-mono']">
+            <p>yarn add @solid-query-devtools</p>
+            <Show when={copyYarn() || copiedYarn()}>
+              <Switch>
+                <Match when={!copiedYarn()}>
+                  <IoCopy class='cursor-pointer' size={18} color={"gray"} title="CopyOutlined" onClick={() => copyText('yarn add @solid-query-devtools', setCopiedYarn)}/>
+                </Match>
+                <Match when={copiedYarn()}>
+                  <FaRegularCircleCheck size={18} color={"green"} title="CopyOutlined"/>
+                </Match>
+              </Switch>
+            </Show>
+          </div>
+          </div>
+          </Show>
+          </div>
+        </div>
         <p class="my-4">
           <span>Home</span>
           {" - "}
