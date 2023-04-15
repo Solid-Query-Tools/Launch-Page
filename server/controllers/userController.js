6c1b82cddd0163b107207639c0b6dbc0f6d087ea
Truett <truett.davis@gmail.com>
2023-04-15 11:08:21 -0400

    cleaned up user server routes and authentication management

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,13 +3,26 @@ const { Session } = require('../models');
 const userController = {
 
   getUser: async (req, res, next) => {
-    console.log("Retrieving the username from the database")
+    console.log("Retrieving the username from the database");
+    // if no session cookie, return null to the client
+    if (!req.cookies.session) {
+      res.locals.userResponse = null;
+      return next();
+    }
+
+    // otherwise search for a session matching the current cookie
     let cookies = req.cookies;
     console.log("These are the cookies!", cookies);
     await Session.findOne({ userId: req.cookies.session })
       .then(results => {
-        res.locals.currentUser = results.username;
-        console.log("This is the username from the retrieval:", res.locals.currentUser)
+        // if no corresponding session is found, return null to the client
+        if (results === null) {
+          res.locals.userResponse = null;
+          return next();
+        }
+        // otherwise save the user's github username to res.locals.userResponse
+        res.locals.userResponse = results.username;
+        console.log("This is the username from the retrieval:", res.locals.userResponse)
         return next();
       })
       .catch((err) => {
